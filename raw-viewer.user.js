40a3b551250502f6a4dab7febf00ff7fed9b52c6
Select Learns <75225979+selectlearns@users.noreply.github.com>
2021-03-02 09:25:38 -0500

    Rearrange functions and remove debug logging

--- raw-viewer.user.js
+++ raw-viewer.user.js
@@ -10,7 +10,65 @@
 
 (function () {
   'use strict';
-  console.log('UserScript loaded');
+
+  const processPost = (node) => {
+    if (node.querySelector('button.raw-button')) return;
+
+    const topicId = node.getAttribute('data-topic-id');
+    const shareButton = node.querySelector('button.share');
+    const postNumber = shareButton && shareButton.getAttribute('data-post-number');
+    if (!topicId || !shareButton || !postNumber) return;
+
+    const rawButton = createRawButton(topicId, postNumber);
+    rawButton.addEventListener('click', handleRawClick);
+
+    shareButton.after(rawButton);
+  };
+
+  const createRawButton = (topicId, postNumber) => {
+    const rawButton = document.createElement('button');
+    rawButton.setAttribute('data-topic-id', topicId);
+    rawButton.setAttribute('data-post-number', postNumber);
+    rawButton.setAttribute('class', 'raw-button widget-button btn-flat no-text btn-icon');
+    rawButton.setAttribute('aria-label', 'get the raw text of this post');
+    rawButton.setAttribute('title', 'get the raw text of this post');
+    rawButton.innerHTML = `<svg class="fa d-icon d-icon-code svg-icon svg-string" xmlns="http://www.w3.org/2000/svg"><use xlink:href="#code"></use></svg>`;
+    return rawButton;
+  };
+
+  const handleRawClick = async (event) => {
+    event.stopPropagation();
+
+    const { topicId, postNumber } = event.currentTarget.dataset;
+    const post = event.currentTarget.closest('div.contents').querySelector('div.cooked');
+    const rawUrl = `https://discuss.codecademy.com/raw/${topicId}/${postNumber}`;
+
+    let text;
+    try {
+      const response = await fetch(rawUrl);
+      if (response.ok) {
+        text = await response.text();
+      } else {
+        text = `Unable to load.\n\nGo to:\n${rawUrl}`;
+      }
+    } catch (error) {
+      text = `Network error encountered.\n\nGo to:\n${rawUrl}`;
+    }
+
+    const pos = post.getBoundingClientRect();
+    showRawView(text, pos.left + window.scrollX, pos.top + window.scrollY, post.clientWidth, post.clientHeight);
+  };
+
+  const showRawView = (text, left, top, width, height) => {
+    const rawView = document.getElementById('raw-view') || insertRawView();
+    rawView.querySelector('#raw-text').value = text;
+    rawView.style.left = `${left}px`;
+    rawView.style.top = `${top}px`;
+    rawView.style.width = `${width}px`;
+    rawView.style.height = `${height}px`;
+    rawView.style.display = 'block';
+    document.body.addEventListener('click', () => rawView.style.display = 'none', { once: true });
+  };
 
   const insertRawView = () => {
     const styles = {
@@ -32,78 +90,10 @@
     return div;
   };
 
-  const showRawView = (text, left, top, width, height) => {
-    const rawView = document.getElementById('raw-view') || insertRawView();
-    rawView.querySelector('#raw-text').value = text;
-    rawView.style.left = `${left}px`;
-    rawView.style.top = `${top}px`;
-    rawView.style.width = `${width}px`;
-    rawView.style.height = `${height}px`;
-    rawView.style.display = 'block';
-    document.body.addEventListener('click', () => rawView.style.display = 'none', { once: true });
-  };
-
-  const handleRawClick = async (event) => {
-    event.stopPropagation();
-
-    const { topicId, postNumber } = event.currentTarget.dataset;
-    const post = event.currentTarget.closest('div.contents').querySelector('div.cooked');
-    console.log(post);
-    const rawUrl = `https://discuss.codecademy.com/raw/${topicId}/${postNumber}`;
-    console.log('Clicked', topicId, postNumber);
-
-    let text;
-    try {
-      const response = await fetch(rawUrl);
-      if (response.ok) {
-        text = await response.text();
-      } else {
-        text = `Unable to load.\n\nGo to:\n${rawUrl}`;
-      }
-    } catch (error) {
-      text = `Network error encountered.\n\nGo to:\n${rawUrl}`;
-    }
-
-    console.log(text);
-    const pos = post.getBoundingClientRect();
-    console.log(pos);
-    showRawView(text, pos.left + window.scrollX, pos.top + window.scrollY, post.clientWidth, post.clientHeight);
-
-  };
-
-  const createRawButton = (topicId, postNumber) => {
-    const rawButton = document.createElement('button');
-    rawButton.setAttribute('data-topic-id', topicId);
-    rawButton.setAttribute('data-post-number', postNumber);
-    rawButton.setAttribute('class', 'raw-button widget-button btn-flat no-text btn-icon');
-    rawButton.setAttribute('aria-label', 'get the raw text of this post');
-    rawButton.setAttribute('title', 'get the raw text of this post');
-    rawButton.innerHTML = `<svg class="fa d-icon d-icon-code svg-icon svg-string" xmlns="http://www.w3.org/2000/svg"><use xlink:href="#code"></use></svg>`;
-    return rawButton;
-  };
-
-  const processPost = (node) => {
-    if (node.querySelector('button.raw-button')) return;
-
-    const topicId = node.getAttribute('data-topic-id');
-    //const shareButton = node.querySelector('nav > div > button.widget-button.btn-flat.share.no-text.btn-icon');
-    const shareButton = node.querySelector('button.share');
-    const postNumber = shareButton && shareButton.getAttribute('data-post-number');
-    //console.log(topicId, postNumber);
-    if (!topicId || !shareButton || !postNumber) return;
-
-    const rawButton = createRawButton(topicId, postNumber);
-    rawButton.addEventListener('click', handleRawClick);
-
-    //shareButton.parentNode.insertBefore(rawButton, shareButton.nextSibling);
-    shareButton.after(rawButton);
-  };
-
   document.querySelectorAll('article').forEach(node => processPost(node));
 
   const observer = new MutationObserver(mutations => {
     for (const mutation of mutations) {
-      //console.log(mutation);
       for (const node of mutation.addedNodes) {
         if (node.querySelectorAll) {
           node.querySelectorAll('article').forEach(node => processPost(node));
